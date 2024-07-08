1b47d4253af53c197221559d6795619ec0214c6f
Glaucco <glauccolima@gmail.com>
2024-07-08 13:15:38 -0300

    Inicializa FavoritesView após carregamento do DOM em index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Importa a classe FavoritesView que gerencia a interface de usuários favoritos.
 import { FavoritesView } from "./FavoritesView.js";
 
-  // Assegura que o código só executa após o carregamento completo do HTML.
+  // Assegura que o código só executa após o carregamento completo do HTML
   // Isso previne erros de manipulação de elementos que ainda não foram carregados.
   document.addEventListener('DOMContentLoaded', () => {
       // Inicializa a visualização dos favoritos no elemento com ID 'app'.
